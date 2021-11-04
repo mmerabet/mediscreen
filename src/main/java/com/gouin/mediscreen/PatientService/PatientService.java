6046d35f84cc2fad5e2c8575981f03ab6bf131d9
cyran <gs.gouin@gmail.com>
2021-11-04 13:26:35 +0100

    initialisation du projet

---
+++ src/main/java/com/gouin/mediscreen/PatientService/PatientService.java
@@ -0,0 +1,4 @@
+package com.gouin.mediscreen.PatientService;
+
+public interface PatientService {
+}
---
+++ src/test/java/com/gouin/mediscreen/MediscreenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gouin.mediscreen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediscreenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
